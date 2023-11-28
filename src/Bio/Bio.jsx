e1f1d7d5ff4ce66e5ede8c1609a65e395486ff6f
juanmachahin <juanma.chahin@gmail.com>
2023-11-27 22:14:12 -0400

    nuevo

--- src/Bio/Bio.jsx
+++ src/Bio/Bio.jsx
@@ -39,7 +39,7 @@ const Bio = () => {
                     business, or tech project.
                     </span>
                     <div className="photo_bio">
-                <img src="public\Images\jmsucre.jpg" className="photo"></img>
+                <img src="\Images\jmsucre.jpg" className="photo"></img>
             </div>
                 </div>
 
