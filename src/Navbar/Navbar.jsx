337c5ddb40448e96108ea9dd01ac55484594b0d7
juanmachahin <juanma.chahin@gmail.com>
2023-11-28 16:24:53 -0400

    últimas correciones mobile first

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -5,15 +5,20 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false)
+
+    const closeMenu = () => {
+        setIsOpen(false);
+    }
+
     return(
         <div className="navbar">
             <div className="nav_logo">  JUAN MANUEL</div>
             <div className={`nav_items ${isOpen && "open"}`}>
-                <Link to="/"> HOME</Link>
-                <Link to="/Bio"> BIO</Link>
-                <Link to="/Portfolio"> PORTFOLIO</Link>
-                <Link to="/Contact"> CONTACT</Link>
-                <Link to="/Music"> MUSIC</Link>
+                <Link to="/" onClick={closeMenu}> HOME</Link>
+                <Link to="/Bio" onClick={closeMenu}> BIO</Link>
+                <Link to="/Portfolio" onClick={closeMenu}> PORTFOLIO</Link>
+                <Link to="/Contact" onClick={closeMenu}> CONTACT</Link>
+                <Link to="/Music" onClick={closeMenu}> MUSIC</Link>
             </div>
             <div className={`nav_toggle ${isOpen && "open"}`} onClick={ () => setIsOpen(!isOpen)}>
                 <span></span>
@@ -24,3 +29,4 @@ const Navbar = () => {
     )
 }
 export default Navbar
+
